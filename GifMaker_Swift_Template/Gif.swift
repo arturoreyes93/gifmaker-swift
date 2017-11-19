d454fd0d7fb63016684f1b8cd325d8b22f9c572c
Arturo Reyes <reyesm93@gmail.com>
2017-11-18 19:05:24 -0600

    Create all files

---
+++ GifMaker_Swift_Template/Gif.swift
@@ -0,0 +1,9 @@
+//
+//  Gif.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Arturo Reyes on 11/18/17.
+//  Copyright © 2017 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
---
+++ GifMaker_Swift_Template/SavedGifsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SavedGifsViewController.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Arturo Reyes on 11/18/17.
+//  Copyright © 2017 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
